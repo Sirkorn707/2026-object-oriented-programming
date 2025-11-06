7b4fa38fdd4fd1686facf0e29cf13bb76476fb74
Kris <Kris@Orn.com>
2025-11-06 15:31:34 +0100

    added ex6 lab6

---
+++ lab06/permutacje-wektora-rekur.cpp
@@ -0,0 +1,32 @@
+/*
+Zadanie 6.
+Napisz program, który za pomocą rekurencyjnej funkcji wypisze na ekranie wszystkie permutacje
+elementów wektora.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void wypiszPermutacje(std::vector<int>& wektor, int start) {
+    if (start == wektor.size() - 1) {
+        for (int i : wektor) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+        return;
+    }
+
+    for (int i = start; i < wektor.size(); ++i) {
+        std::swap(wektor[start], wektor[i]);
+        wypiszPermutacje(wektor, start + 1);
+        std::swap(wektor[start], wektor[i]);
+    }
+}
+
+int main() {
+    std::vector<int> wektor = {1, 2, 3};
+    std::cout << "Permutacje wektora: " << std::endl;
+    wypiszPermutacje(wektor, 0);
+    return 0;
+}
